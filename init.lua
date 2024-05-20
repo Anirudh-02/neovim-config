5a64ddaa426a3bc4fb03ddc65762ab0805add679
Anirudh <anirudhshukla02@gmail.com>
2024-05-20 13:12:22 +0530

    Add nodesetup as comment

--- init.lua
+++ init.lua
@@ -1,6 +1,16 @@
 -- Store global Vim in a local vim variable, doing this just to avoid lsp warnings
 local vim = vim
 
+-- Nodesetup for projects using old Node.js
+-- vim.env.NEOVIM_NODE_VERSION = 'v18.16.0'
+--
+-- if vim.fn.has('unix') and vim.env.NEOVIM_NODE_VERSION then
+--     local node_dir = vim.env.HOME .. '/.nvm/versions/node/' .. vim.env.NEOVIM_NODE_VERSION .. '/bin/'
+--     if (vim.fn.isdirectory(node_dir)) then
+--         vim.env.PATH = node_dir .. ':' .. vim.env.PATH
+--     end
+-- end
+
 -- Setup Lazy package manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
